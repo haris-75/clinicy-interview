6d498598e549e37a4c1042b9c65dce3384906564
haris-75 <bitworld75@gmail.com>
2024-07-14 15:11:15 +0500

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+	return (
+		<h1 className="text-3xl font-bold">
+			React + Vite + Tailwindcss
+			<span className="text-2xl font-medium">Starter</span>
+		</h1>
+	);
+}
+
+export default App;
